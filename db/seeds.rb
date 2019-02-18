2ee3876ae73b84fb4b6e9f4e1bd5e29a17ec7e4d
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-18 18:43:04 +0800

    Setting up seed file to read CSV file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'worldcities.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+puts csv_text
