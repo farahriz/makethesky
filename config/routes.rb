76bbeb09d491e4dec56eace5ccaddfd9fb89a29d
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-19 15:48:28 +0800

    Fix route formatting

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  get ‘signup’, to: ‘users#new’, as: ‘signup’
-  get ‘login’, to: ‘sessions#new’, as: ‘login’
-  get ‘logout’, to: ‘sessions#destroy’, as: ‘logout’
+  get 'signup', to: 'users#new', as: ‘signup’
+  get 'login', to: 'sessions#new', as: ‘login’
+  get 'logout', to: 'sessions#destroy', as: ‘logout’
 
   resources :scarves
-  
+
   root "landing#index"
 
   get 'patterns/new'
