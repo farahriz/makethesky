7436ce2663a04092304cf4dc035064a2b560ac3c
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-19 17:36:26 +0800

    Added helper to find current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_user
+
+	def current_user
+		if session[:user_id]
+		    @current_user ||= User.find(session[:user_id])
+		else
+		    @current_user = nil
+		end
+	end
+
+	def signed_in?
+		!@current_user.nil?
+	end
 end
