bb2af6886346ec9b36b3ea4e8ab4f6ee0c0f6bb1
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-21 23:44:24 +0800

    Added scopes to scarf model

--- app/models/scarf.rb
+++ app/models/scarf.rb
@@ -1,4 +1,7 @@
 class Scarf < ApplicationRecord
 	belongs_to :city
 	belongs_to :user
+
+	scope :scarf_name, -> (scarf_name) { where("title ILIKE ?" , "%#{scarf_name}%")   }
+    scope :descrip, -> (description) { where("description ILIKE ?", "%#{description}%" ) }
 end
